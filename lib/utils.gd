36b5e6cfb94904f84af84a42acedacb702e896d1
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2019-01-23 18:13:35 +0100

    document utility functions

--- lib/utils.gd
+++ lib/utils.gd
@@ -1,7 +1,55 @@
+#! @Chapter Utility functions
+
+#! @Section Miscellaneous
+
+#! @Arguments blocks
+
+#! @Returns Matrix given by putting the given matrix <A>blocks</A> on
+#! the diagonal
 DeclareGlobalFunction( "BlockDiagonalMatrix" );
-DeclareGlobalFunction( "Replicate" );
-DeclareGlobalFunction( "TensorProductRepLists" );
+
+#! @Arguments hom, func
+
+#! @Returns Homomorphism g given by g(x) = func(hom(x)).
+
+#! @Description This is mainly for convenience, since it handles all
+#! GAP accounting issues regarding the range, ByImages vs ByFunction,
+#! etc.
 DeclareGlobalFunction( "ComposeHomFunction" );
+
+#! @Arguments elem, n
+
+#! @Returns List of <A>n</A> copies of <A>elem</A>
+DeclareGlobalFunction( "Replicate" );
+
+#! @Section Representation theoretic functions
+
+#! @Arguments list1, list2
+
+#! @Returns All possible tensor products given by $\rho \otimes \tau$
+#! where $\rho$ is taken from <A>list1</A> and $\tau$ is taken from
+#! <A>list2</A>.
+DeclareGlobalFunction( "TensorProductRepLists" );
+
+#! @Arguments list
+
+#! @Returns Direct sum of the list of representations <A>list</A>
 DeclareGlobalFunction( "DirectSumRepList" );
+
+#! @Arguments rho, tau
+
+#! @Returns true if <A>rho</A> and <A>tau</A> are isomorphic as
+#! representations, false otherwise.
+
+#! @Description Note that two representations are isomorphic iff they
+#! give similar matrices, also iff they have the same irreducible
+#! decomposition. We use characters to determine the latter: the first
+#! is too expensive to check for large matrices.
 DeclareGlobalFunction( "AreRepsIsomorphic" );
+
+#! @Arguments rho
+
+#! @Returns Degree of the representation <A>rho</A>. That is,
+#! $\mbox{Tr}(\rho(e_G))$, where $e_G$ is the identity of the group
+#! $G$ that <A>rho</A> has as domain.
 DeclareGlobalFunction( "DegreeOfRepresentation" );
