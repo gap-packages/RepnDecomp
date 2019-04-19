abc1daa72137b91786af856c73f15a44f17204b7
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2019-04-19 23:55:59 +0100

    add main manual page to set up chapters etc

---
+++ lib/aaa_manual.gd
@@ -0,0 +1,59 @@
+#! @Chapter Introduction
+
+#! @Section Getting started with RepnDecomp
+
+#! This package allows computations of various decompositions of a
+#! representation $\rho : G \to GL(V)$ where $G$ is finite and $V$ is
+#! a finite-dimensional $\mathbb{C}$-vector space.
+
+#! The algorithms implemented can be divided into two groups: methods
+#! due to Serre from his book Linear Representations of Finite Groups,
+#! and original methods due to the authors of this package.
+#!
+#! The default is to use the algorithms due to Serre. If you pass the
+#! option `method := "alternate"` to a function, it will use the
+#! alternate method. Passing the option `parallel` will try to compute
+#! in parallel as much as possible. See the individual functions for
+#! options you can pass.
+#!
+#! The main functions implemented in this package are:
+#!
+#! For decomposing representations into canonical and irreducible
+#! direct summands:
+#!
+#! * <Ref Func="CanonicalDecomposition" />
+#! * <Ref Func="IrreducibleDecomposition" />
+#! * <Ref Func="IrreducibleDecompositionCollected" />
+#!
+#! For block diagonalising representations:
+#!
+#! * <Ref Func="BlockDiagonalBasisOfRepresentation" />
+#! * <Ref Func="BlockDiagonalRepresentation" />
+#!
+#! For computing centraliser rings:
+#!
+#! * <Ref Func="CentralizerBlocksOfRepresentation" />
+#! * <Ref Func="CentralizerOfRepresentation" />
+#!
+#! For testing isomorphism and computing isomorphisms (intertwining
+#! operators) between representations:
+#!
+#! * <Ref Func="LinearRepresentationIsomorphism" />
+#! * <Ref Func="AreRepsIsomorphic" />
+#! * <Ref Func="IsLinearRepresentationIsomorphism" />
+#!
+#! For testing unitarity of representations and the unitarisation of
+#! representations:
+#!
+#! * <Ref Func="UnitaryRepresentation" />
+#! * <Ref Func="IsUnitaryRepresentation" />
+
+#! @Chapter Decomposing representations into irreducibles
+
+#! @Chapter Centraliser (commutant) rings
+
+#! @Chapter Isomorphisms between representations
+
+#! @Chapter Algorithms for unitary representations
+
+#! @Chapter Miscellaneous useful functions
